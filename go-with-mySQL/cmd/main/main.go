51fa7b46337ba990171659d01ca0d38cd0c2296d
Tejas Pawar <tejasrpawar0108@gmail.com>
2024-06-15 11:14:30 -0600

    First commit | MYSQL + Go

---
+++ go-with-mySQL/cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/Go-projects/go-with-mySQL/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+
+}
